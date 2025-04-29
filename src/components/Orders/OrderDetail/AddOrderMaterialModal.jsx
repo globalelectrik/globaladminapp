d68e05b2c799b5e96a128c03b9a5e25d4b249b2c
Nahum Jaime <nahum.jsz@gmail.com>
2025-04-29 20:46:40 +0200

    anadirMaterial en orden done

--- src/components/Orders/OrderDetail/AddOrderMaterialModal.jsx
+++ src/components/Orders/OrderDetail/AddOrderMaterialModal.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Dialog } from "@headlessui/react"; // puedes usar modal propio si no usas HeadlessUI
+import Select from "react-select";
+import { Dialog } from "@headlessui/react";
 import useGet from "../../../hooks/useGet/useGet";
-import usePost from './../../../hooks/usePost/usePost';
+import usePost from "../../../hooks/usePost/usePost";
 import usePut from "../../../hooks/usePut/usePut";
 
 export default function AddOrderMaterialModal({ isOpen, onClose, orderSelected, setOrderSelected }) {
-  
   const [materialData, setMaterialData] = useState({
     materialName: "",
     materialReference: "",
@@ -16,38 +16,29 @@ export default function AddOrderMaterialModal({ isOpen, onClose, orderSelected, 
     materialClientReference: "",
   });
 
-    const {
-      data: classificationsData,
-      isLoading: classificationsIsLoading,
-      error: classificationsError,
-      fetchGet: classificationsFetchGet,
-    } = useGet();
-  
-      const {
-      data: brandsData,
-      isLoading: brandsIsLoading,
-      error: brandsError,
-      fetchGet: brandsFetchGet,
-    } = useGet();
+  const {
+    data: classificationsData,
+    fetchGet: classificationsFetchGet,
+  } = useGet();
 
-    const { 
-      putResponse: orderAddMaterialUpdatedData,
-      isLoading: orderAddMaterialUpdateIsLoading,
-      error: orderAddMaterialUpdateError,
-      fetchPut: orderAddMaterialUpdateFetchPut,
-      } = usePut();
-  
+  const {
+    data: brandsData,
+    fetchGet: brandsFetchGet,
+  } = useGet();
 
+  const {
+    putResponse: orderAddMaterialUpdatedData,
+    fetchPut: orderAddMaterialUpdateFetchPut,
+  } = usePut();
 
   const { fetchPost } = usePost();
 
   useEffect(() => {
-    if(isOpen){
+    if (isOpen) {
       brandsFetchGet("/materials/getBrands");
       classificationsFetchGet("/materials/getClassifications");
     }
   }, [isOpen]);
-
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -56,65 +47,108 @@ export default function AddOrderMaterialModal({ isOpen, onClose, orderSelected, 
 
   const handleSave = async () => {
     const { quantity, materialClientReference, ...newMaterialData } = materialData;
-
-    // Crear el nuevo material
-    const response = await orderAddMaterialUpdateFetchPut(`/orders/updateAddMaterialToOrder/${orderSelected.id}`, newMaterialData);
-    const createdMaterial = response?.material;
-
-    if (!createdMaterial) return alert("Error al crear el material");
-
-    // Agregarlo a la orden
-    const newMaterialRow = {
-      material: createdMaterial.id,
-      quantity: parseInt(quantity),
-      materialClientReference,
-      delivered: false,
-      deliveries: [],
-      purchases: [],
-      supplier: null,
-    };
-
-    setOrderSelected((prev) => ({
-      ...prev,
-      materials: [...prev.materials, newMaterialRow],
-    }));
-
+     await orderAddMaterialUpdateFetchPut(`/orders/updateAddMaterialToOrder/${orderSelected.id}`, materialData);
     onClose();
   };
 
+  useEffect(() => {
+    if (orderAddMaterialUpdatedData?.message === "Orden actualizada con éxito") {
+      setOrderSelected(orderAddMaterialUpdatedData.order)
+      return alert("Orden actualizada con éxito");
+    } if (orderAddMaterialUpdatedData?.message === "Error al actualizar Orden") {
+      return alert("Error al actualizar Orden");
+    }
+  }, [orderAddMaterialUpdatedData]);
+
+  const brandOptions = brandsData?.brands?.map((b) => ({
+    label: b.brandName,
+    value: b.id,
+  })) || [];
+
+  const classificationOptions = classificationsData?.classifications?.map((c) => ({
+    label: c.classificationName,
+    value: c.id,
+  })) || [];
+
   return !isOpen ? null : (
     <div className="fixed inset-0 bg-black/50 z-50 flex justify-center items-center">
-      <div className="bg-white p-6 rounded shadow-md w-full max-w-2xl space-y-4">
+      <div className="bg-white p-6 rounded shadow-md w-full max-w-2xl space-y-2">
         <h2 className="text-xl font-bold">Crear y Añadir Material</h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <input name="materialName" value={materialData.materialName} onChange={handleChange} placeholder="Nombre del material" className="border px-3 py-2 rounded" />
-          <input name="materialReference" value={materialData.materialReference} onChange={handleChange} placeholder="Referencia" className="border px-3 py-2 rounded" />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 text-sm">
+          <input
+            name="materialName"
+            value={materialData.materialName}
+            onChange={handleChange}
+            placeholder="Nombre del material"
+            className="border px-3 py-2 rounded"
+          />
+          <input
+            name="materialReference"
+            value={materialData.materialReference}
+            onChange={handleChange}
+            placeholder="Referencia"
+            className="border px-3 py-2 rounded"
+          />
 
-          <select name="materialBrand" value={materialData.materialBrand} onChange={handleChange} className="border px-3 py-2 rounded" size={3}>
-            {brandsData?.brands?.map((b) => (
-              <option key={b.id} value={b.id}>{b.brandName}</option>
-            ))}
-          </select>
+          <div className="flex flex-col">
+            <label className="mb-1">Marca</label>
+            <Select
+              className="text-sm"
+              options={brandOptions}
+              value={brandOptions.find(opt => opt.value === materialData.materialBrand) || null}
+              onChange={(selected) =>
+                setMaterialData((prev) => ({ ...prev, materialBrand: selected?.value || "" }))
+              }
+            />
+          </div>
 
-          <select name="materialClassification" value={materialData.materialClassification} onChange={handleChange} className="border px-3 py-2 rounded" size={3} >
-           
-            {classificationsData?.classifications?.map((c) => (
-              <option key={c.id} value={c.id}>{c.classificationName}</option>
-            ))}
-          </select>
+          <div className="flex flex-col">
+            <label className="mb-1">Clasificación</label>
+            <Select
+              className="text-sm"
+              options={classificationOptions}
+              value={classificationOptions.find(opt => opt.value === materialData.materialClassification) || null}
+              onChange={(selected) =>
+                setMaterialData((prev) => ({ ...prev, materialClassification: selected?.value || "" }))
+              }
+            />
+          </div>
 
-          <select name="materialStatusType" value={materialData.materialStatusType} onChange={handleChange} className="border px-3 py-2 rounded">
-            <option value="Nuevo">Nuevo</option>
-            <option value="Reacondicionado">Reacondicionado</option>
-          </select>
+          <div className="flex flex-col">
+            <label>Estado</label>
+            <select
+              name="materialStatusType"
+              value={materialData.materialStatusType}
+              onChange={handleChange}
+              className="border px-3 py-2 rounded"
+            >
+              <option className="text-sm" value="Nuevo">Nuevo</option>
+              <option className="text-sm" value="Reacondicionado">Reacondicionado</option>
+            </select>
+          </div>
 
-          <input name="quantity" value={materialData.quantity} onChange={handleChange} type="number" placeholder="Cantidad" className="border px-3 py-2 rounded" />
+          <div className="flex flex-col">
+            <label>Cantidad</label>
+            <input
+              name="quantity"
+              value={materialData.quantity}
+              onChange={handleChange}
+              type="number"
+              placeholder="Cantidad"
+              className="border px-3 py-2 rounded"
+            />
+          </div>
         </div>
 
-        <div>
-          <label className="font-medium">Referencia Cliente</label>
-          <input name="materialClientReference" value={materialData.materialClientReference} onChange={handleChange} className="w-full border px-3 py-2 rounded mt-1" />
+        <div className="text-sm">
+          <label>Referencia Cliente</label>
+          <input
+            name="materialClientReference"
+            value={materialData.materialClientReference}
+            onChange={handleChange}
+            className="w-full border px-3 py-2 rounded"
+          />
         </div>
 
         <div className="flex justify-end space-x-2">
